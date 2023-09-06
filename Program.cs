42d4b2bfce9dd297572937cae83672d6ea1682d7
ayseglozgr <a.ozgurr0@gmail.com>
2023-09-06 12:41:17 +0300

    bug fixes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace YazilimSinaviSoru1
 {
@@ -17,67 +18,51 @@ namespace YazilimSinaviSoru1
                 new Point(-5, 2),
                 new Point(1, 5),
                 new Point(3, -5)
+                //new Point(2, -5),
+                //new Point(2, 7),
+                //new Point(5, 7),
+                //new Point(5, 8),
+                //new Point(3, 9)
+
             };
-           List<Point> otherPointsY = new List<Point>();
-           List<Point> otherPointsX = new List<Point>();
+            List<Point> otherPointsY = new(); 
+            List<Point> otherPointsX = new();
 
-            // Y eksenine paralel doğruların geçtiği noktaları buluyoruz. aynı zamanda bu noktaları bir listede tutuyoruz.
-          
-            int yAxisParallelism = 0;
-            for (int i = 0; i < noktalar.Length; i++)
-            {
-                for (int j = i+1; j < noktalar.Length; j++)
-                {
-                    if (noktalar[i].x == noktalar[j].x)
-                    {
-                        yAxisParallelism++;
-                        otherPointsY.Add(new Point(noktalar[i].x, noktalar[j].y));
-                    }
-                }
-            }
-            // Bu döngüde aynı doğru üzerinde 2 den fazla nokta varsa fazla olanları listeden çıkarıyoruz. 
-            for (int i = 0; i < otherPointsY.Count; i++)
-            {
-                for (int j = i + 1; j < otherPointsY.Count; j++)
-                {
-                    if (otherPointsY[i].x == otherPointsY[j].x)
-                    {
-                        otherPointsY.RemoveAt(j);
-                        yAxisParallelism--;
-                    }
-                }
-            }
-            // X eksenine paralel doğruların geçtiği noktaları buluyoruz. aynı zamanda bu noktaları bir listede tutuyoruz.
-
-            int xAxisParallelism = 0;
             for (int i = 0; i < noktalar.Length; i++)
             {
                 for (int j = i + 1; j < noktalar.Length; j++)
                 {
-                    if (noktalar[i].y == noktalar[j].y)
+                    //y eksenine paralel olan doğrular üzerinde olan noktaları (eğer aynı doğru üzerinde birden fazla nokta varsa bir tanesini) ekliyoruz.
+                    //Dolayısıyla bize çıktı olarak y ekenine paralel kaç tane doğru olduğu sonucunu verecektir.
+                    if ((noktalar[i].x == noktalar[j].x) && !otherPointsY.Any(a => a.x == noktalar[j].x))
                     {
-                        xAxisParallelism++;
+                        otherPointsY.Add(new Point(noktalar[i].x, noktalar[i].y));
+                    }
+                }
+            }
+            
+            for (int i = 0; i < noktalar.Length; i++)
+            {
+                for (int j = i + 1; j < noktalar.Length; j++)
+                {
+                    //x eksenine paralel olan doğrular üzerinde olan noktaları (eğer aynı doğru üzerinde birden fazla nokta varsa bir tanesini) ekliyoruz.
+                    //Dolayısıyla bize çıktı olarak x ekenine paralel kaç tane doğru olduğu sonucunu verecektir.
+                    if ((noktalar[i].y == noktalar[j].y) && !otherPointsX.Any(a => a.y == noktalar[j].y))
+                    {
                         otherPointsX.Add(new Point(noktalar[i].x, noktalar[j].y));
                     }
                 }
             }
-            // Bu döngüde aynı doğru üzerinde 2 den fazla nokta varsa fazla olanları listeden çıkarıyoruz. 
-            for (int i = 0; i < otherPointsX.Count; i++)
+
+            Console.WriteLine("Elimizdeki noktalar :");
+
+            for (int i = 0; i < noktalar.Length; i++)
             {
-                for (int j = i + 1; j < otherPointsX.Count; j++)
-                {
-                    if (otherPointsX[i].y == otherPointsX[j].y)
-                    {
-                        otherPointsX.RemoveAt(j);
-                        xAxisParallelism--;
-                    }
-                }
-
+                Console.WriteLine("(" + noktalar[i].x + "," +noktalar[i].y + ")");
             }
-
             // Sonuçları yazdıralım
-            Console.WriteLine("Y eksenine paralel doğru sayısı: {0}", yAxisParallelism);
-            Console.WriteLine("X eksenine paralel doğru sayısı: {0}", xAxisParallelism);
+            Console.WriteLine("Y eksenine paralel doğru sayısı: {0}", otherPointsY.Count);
+            Console.WriteLine("X eksenine paralel doğru sayısı: {0}", otherPointsX.Count);
         }
     }
     class Point
@@ -92,3 +77,5 @@ namespace YazilimSinaviSoru1
         }
     }
 }
+
+            
